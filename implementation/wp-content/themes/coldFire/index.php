bca96073b8f169890709b8b57ca413143358b867
Tim Visher <tim.visher@gmail.com>
2008-09-27 03:27:29 +0000

    Massive editing of style sheet and markup. I need to remember to check in more granularly...

--- implementation/wp-content/themes/coldFire/index.php
+++ implementation/wp-content/themes/coldFire/index.php
@@ -1,23 +1,23 @@
 <?php
 	echo '<?xml version="1.0" encoding="ISO-8859-1" ?>';
-	$img_dir = get_bloginfo('template_directory');
+	$template_dir = get_bloginfo('template_directory');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 <title>Burning Ones - Home</title>
-<link rel="stylesheet" type="text/css" media="screen" href="burningones.css" />
+<link rel="stylesheet" type="text/css" media="screen" href=<?php echo "\"$template_dir/"; ?>style.css" />
 </head>
 <body>
 	<div id="header">
 		<h1 id="titling">Burning Ones</h1>
-		<img id="headerImage" src=<?php echo "\"$img_dir/images/header/header1.jpg\""; ?> width="95%" title="Burning Ones" />
+		<img id="headerImage" src=<?php echo "\"$template_dir/images/header/header1.jpg\""; ?> title="Burning Ones" />
 	</div>
 	<div id="body">
 		<div id="post">
 			<h2 id="postTitle">Lorem Ipsum</h2>
-			<b id="postAuthor">Tim Visher</b>
+			<p id="postAuthor">Tim Visher</p>
 			<div id="postContent">
 				<h3>Lorem</h3>
 				<p>Lorem <a href="">ipsum</a> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex <a href="">ea commodo consequat</a>. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -31,7 +31,7 @@
 			<p id="comments"><a href="">3 Comments</a></p>
 		</div>
 		<div id="sidebar">
-			<a href="http://feeds.feedburner.com/burningonesblog" id="rssFeedLink"><img src=<?php echo "\"$img_dir/images/rss.png\""; ?> title="Burning Ones RSS Feed" /></a>
+			<a href="http://feeds.feedburner.com/burningonesblog" id="rssFeedLink"><img src=<?php echo "\"$template_dir/images/rss.png\""; ?> title="Burning Ones RSS Feed" /></a>
 			<p id="siteSlug">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 			<ul id="postNav">
 				<li>Previously: <a href="">Ipsum</a></li>
@@ -40,14 +40,15 @@
 		</div>
 	</div>
 	<div id="footer">
-		<ul id="blogRoll">
+		<div id="linkerific">
+		<ul id="blogRoll" class="block">
 			<li><a href="">Lorem</a></li>
 			<li><a href="">Ipsum</a></li>
 			<li><a href="">Dolor</a></li>
 			<li><a href="">Sit</a></li>
 			<li><a href="">Amet</a></li>
 		</ul>
-		<ul id="recently">
+		<ul id="recently" class="block">
 			<li id="mostPopularPost"><a href="">Dolor</a></li>
 			<ul id="recentPosts">
 				<li><a href="">Lorem</a></li>
@@ -57,7 +58,7 @@
 				<li><a href="">Amet</a></li>
 			</ul>
 		</ul>
-		<ul id="elsewhere">
+		<ul id="elsewhere" class="block">
 			<ul id="delicious">
 				<li><a href="">Lorem</a></li>
 				<li><a href="">Ipsum</a></li>
@@ -76,7 +77,8 @@
 				
 			</ul>
 		</ul>
-		<p id="copyright">&copy; Burning Ones 2007-2008</p>
+		</div>
 	</div>
+	<p id="copyright">&copy; Burning Ones 2007-2008</p>
 </body>
 </html>
