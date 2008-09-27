d167f0b606d29d6f75251e1b580b98fc383409e6
Tim Visher <tim.visher@gmail.com>
2008-09-27 21:57:45 +0000

    Worked on the siteSlug paragraph and made it serif w/ italic font and higher leading.

--- implementation/wp-content/themes/coldFire/index.php
+++ implementation/wp-content/themes/coldFire/index.php
@@ -19,7 +19,6 @@
 			<h2 id="postTitle">Lorem Ipsum</h2>
 			<p id="postAuthor">Tim Visher</p>
 			<div id="postContent">
-				<h3>Lorem</h3>
 				<p>Lorem <a href="">ipsum</a> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex <a href="">ea commodo consequat</a>. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<h3>Ipsum</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
