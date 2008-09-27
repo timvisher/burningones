44eecdc391adcf28663d3dd04b6bcceb1b1c2770
Tim Visher <tim.visher@gmail.com>
2008-09-27 14:52:27 +0000

    Worked on some formatting and the twitter section.

--- implementation/wp-content/themes/coldFire/index.php
+++ implementation/wp-content/themes/coldFire/index.php
@@ -61,12 +61,13 @@
 		<ul id="elsewhere" class="block">
 			<ul id="delicious">
 				<li>
-					<script type="text/javascript" src="http://feeds.delicious.com/v2/js/stopsineman?title=%40Delicious&icon=s&count=5&sort=date&tags&extended&name&showadd"></script>
+					<script type="text/javascript" src="http://feeds.delicious.com/v2/js/stopsineman?title=%40Delicious&count=5&sort=date&extended"></script>
 				</li>
 			</ul>
 			<ul id="twitter">
 				<li>
 					<div id="twitter_div">
+					<h2><a href="http://twitter.com/stopsineman">@Twitter</a></h2>
 					<ul id="twitter_update_list"></ul>
 					</div>
 				</li>
@@ -74,24 +75,16 @@
 			<ul id="flickr">
 				<li>
 					<!-- Start of Flickr Badge -->
-					<style type="text/css">
-					#flickr_badge_source_txt {padding:0; font: 11px Arial, Helvetica, Sans serif; color:#cccccc;}
-					#flickr_badge_icon {display:block !important; margin:0 !important; border: 1px solid rgb(0, 0, 0) !important;}
-					#flickr_icon_td {padding:0 5px 0 0 !important;}
-					.flickr_badge_image {text-align:center !important;}
-					.flickr_badge_image img {border: 1px solid black !important;}
-					#flickr_www {display:block; padding:0 10px 0 10px !important; font: 11px Arial, Helvetica, Sans serif !important; color:#3993ff !important;}
-					#flickr_badge_uber_wrapper a:hover,
-					#flickr_badge_uber_wrapper a:link,
-					#flickr_badge_uber_wrapper a:active,
-					#flickr_badge_uber_wrapper a:visited {text-decoration:none !important; background:inherit !important;color:#cccccc;}
-					#flickr_badge_wrapper {}
-					#flickr_badge_source {padding:0 !important; font: 11px Arial, Helvetica, Sans serif !important; color:#cccccc !important;}
-					</style>
-					<table id="flickr_badge_uber_wrapper" cellpadding="0" cellspacing="10" border="0"><tr><td><a href="http://www.flickr.com" id="flickr_www">www.<strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong>.com</a><table cellpadding="0" cellspacing="10" border="0" id="flickr_badge_wrapper">
-					<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=5&display=latest&size=t&layout=v&source=user&user=7155116%40N04"></script>
+					<table id="flickr_badge_uber_wrapper" cellpadding="0" cellspacing="10" border="0">
+						<tr>
+							<td>
+								<a href="http://www.flickr.com" id="flickr_www">www.<strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong>.com</a>
+								<table cellpadding="0" cellspacing="10" border="0" id="flickr_badge_wrapper">
+									<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=5&display=latest&size=t&layout=v&source=user&user=7155116%40N04"></script>
+								</table>
+							</td>
+						</tr>
 					</table>
-					</td></tr></table>
 					<!-- End of Flickr Badge -->
 				</li>
 			</ul>
