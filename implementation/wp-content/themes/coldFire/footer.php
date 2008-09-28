add216e8f231c43664c6d5d167e7c78e063a3290
Tim Visher <tim.visher@gmail.com>
2008-09-28 12:30:14 +0000

    Moved footer into footer.php.

---
+++ implementation/wp-content/themes/coldFire/footer.php
@@ -0,0 +1,59 @@
+	<div id="footer">
+		<ul id="liens" class="block">
+			<h2>Liens</h2>
+			<li>Lorem&nbsp;<a href="">&raquo;</a></li>
+			<li>Ipsum&nbsp;<a href="">&raquo;</a></li>
+			<li>Dolor&nbsp;<a href="">&raquo;</a></li>
+			<li>Sit&nbsp;<a href="">&raquo;</a></li>
+			<li>Amet&nbsp;<a href="">&raquo;</a></li>
+		</ul>
+		<ul id="recently" class="block">
+			<h2>Recently</h2>
+			<li id="mostPopularPost">Dolor&nbsp;<a href="">&raquo;</a></li>
+			<ul id="recentPosts">
+				<li>Lorem&nbsp;<a href="">&raquo;</a></li>
+				<li>Ipsum&nbsp;<a href="">&raquo;</a></li>
+				<li>Dolor&nbsp;<a href="">&raquo;</a></li>
+				<li>Sit&nbsp;<a href="">&raquo;</a></li>
+				<li>Amet&nbsp;<a href="">&raquo;</a></li>
+			</ul>
+		</ul>
+		<ul id="elsewhere" class="block">
+			<ul id="delicious">
+				<li>
+					<script type="text/javascript" src="http://feeds.delicious.com/v2/js/stopsineman?title=%40Delicious&count=5&sort=date&extended"></script>
+				</li>
+			</ul>
+			<ul id="twitter">
+				<li>
+					<div id="twitter_div">
+					<h2><a href="http://twitter.com/stopsineman">@Twitter</a></h2>
+					<ul id="twitter_update_list"></ul>
+					</div>
+				</li>
+			</ul>
+			<ul id="flickr">
+				<li>
+					<!-- Start of Flickr Badge -->
+					<table id="flickr_badge_uber_wrapper" cellpadding="0" cellspacing="10" border="0">
+						<tr>
+							<td>
+								<a href="http://www.flickr.com" id="flickr_www">www.<strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong>.com</a>
+								<table cellpadding="0" cellspacing="10" border="0" id="flickr_badge_wrapper">
+									<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=5&display=latest&size=t&layout=v&source=user&user=7155116%40N04"></script>
+								</table>
+							</td>
+						</tr>
+					</table>
+					<!-- End of Flickr Badge -->
+				</li>
+			</ul>
+		</ul>
+	</div>
+	<p id="copyright">&copy; Burning Ones 2007-2008</p>
+	<!-- Twitter Scripts after page load! -->
+	<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+	<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/stopsineman.json?callback=twitterCallback2&amp;count=1"></script>
+	<!-- End Twitter Scripts -->
+</body>
+</html>
