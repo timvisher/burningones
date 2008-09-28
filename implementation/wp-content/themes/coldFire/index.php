1de7125a2798f83da67ab5e3d195e1c35c1f6bbb
Tim Visher <tim.visher@gmail.com>
2008-09-28 13:14:46 +0000

    Added the Loop. If there are no posts, something will be displayed other than an error now.

--- implementation/wp-content/themes/coldFire/index.php
+++ implementation/wp-content/themes/coldFire/index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 	<div id="post">
+		<?php if (have_posts()) : ?>
 		<h2 id="postTitle">Lorem Ipsum</h2>
 		<p id="postAuthor">Tim Visher</p>
 		<div id="postContent">
@@ -13,6 +14,9 @@
 			<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><blockquote><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></blockquote></blockquote>
 		</div>
 		<p id="postErrata">18 July @ 9:30pm - <a href="">3 Comments</a></p>
+		<?php else : ?>
+		<h2>No Posts Here&hellip;</h2>
+		<?php endif; ?>
 	</div>
 
 <?php get_sidebar(); ?>
