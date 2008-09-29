ee383373bdee70533f057f512160b1b6e4d57625
Tim Visher <tim.visher@gmail.com>
2008-09-29 21:45:17 +0000

    Fixed Validation Warning. Corrected the character encoding.

--- implementation/wp-content/themes/coldFire/header.php
+++ implementation/wp-content/themes/coldFire/header.php
@@ -1,10 +1,10 @@
 <?php
-	echo '<?xml version="1.0" encoding="ISO-8859-1" ?>';
+	echo '<?xml version="1.0" encoding="utf-8" ?>';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Burning Ones - Home</title>
 <link rel="stylesheet" type="text/css" media="screen" href="<?php templateFile("style.css"); ?>" />
 </head>
