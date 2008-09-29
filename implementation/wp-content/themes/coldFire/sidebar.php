22ca101234c173c00a033ab4823d4d6f89f56b55
Tim Visher <tim.visher@gmail.com>
2008-09-29 17:19:04 +0000

    Fixed Validation Error. Malformed img tag.

--- implementation/wp-content/themes/coldFire/sidebar.php
+++ implementation/wp-content/themes/coldFire/sidebar.php
@@ -1,5 +1,5 @@
 	<div id="sidebar">
-		<a href="http://feeds.feedburner.com/burningonesblog" id="rssFeedLink"><img src="<?php templateFile("images/rss.png"); ?>" title="Burning Ones RSS Feed" /></a>
+		<a href="http://feeds.feedburner.com/burningonesblog" id="rssFeedLink"><img src="<?php templateFile("images/rss.png"); ?>" alt="Burning Ones RSS Feed." title="Burning Ones RSS Feed" /></a>
 		<p id="siteSlug"><?php bloginfo('description'); ?></p>
 		<ul id="postNav">
 			<li><a href="">&laquo;</a>&nbsp;Ipsum</li>
